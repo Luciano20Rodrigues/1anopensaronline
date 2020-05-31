26068425a09d10d6dcc7c22901dc82f17cb1a93b
Luciano20Rodrigues <62914253+Luciano20Rodrigues@users.noreply.github.com>
2020-05-31 17:52:54 -0300

    Add files via upload

---
+++ backend/app/controllers/rooms.controller.js
@@ -0,0 +1,68 @@
+import fs from "fs";
+import uuid from "uuid/v4";
+import path from "path";
+
+const roomFilePath = "../file/matrix.room.web.json";
+
+const fetchFromFile = () => {
+  const roomFileExists = fs.existsSync(roomFilePath);
+  if (!roomFileExists) {
+    createRoomFileSync();
+  }
+
+  const roomsData = fs.readFileSync(roomFilePath);
+  const roomsDetail = JSON.parse(roomsData);
+
+  return new Promise(resolve => resolve(roomsDetail));
+};
+
+const createRoomFileSync = () => {
+  const roomsData = [];
+
+  roomsData[0] = {
+    id: uuid(),
+    name: "Bem Vindo a plataforma   PENSAR ONLINE ",
+    disableMeeting: true,
+  };
+
+  const niceNames = [
+    "1 ANO - Sala1",
+    "1 ANO - Sala2",
+    "1 ANO - Sala3",
+    "1 ANO - Sala4",
+    "1 ANO - Sala5",
+    "Diretoria",
+    "Coordenacao Pedagogica",
+    "Reuniao de Professores",
+    "Reuniao de Pais",
+  ];
+
+  for (const niceName of niceNames) {
+    roomsData.push({
+      id: uuid(),
+      name: niceName
+    });
+  }
+
+  fs.mkdirSync(path.dirname(roomFilePath), { recursive: true });
+  fs.writeFileSync(roomFilePath, JSON.stringify(roomsData));
+};
+
+const fetchFromEnvironment = (env) => {
+  const roomsData = env.ROOMS_DATA;
+  const roomsDetail = JSON.parse(roomsData);
+
+  return new Promise(resolve => resolve(roomsDetail));
+};
+
+const fetchRooms = (strategy) => {
+  switch (strategy) {
+    // TODO add suport to fetch from endpoint
+    case "ENVIRONMENT":
+      return fetchFromEnvironment(process.env);
+    default:
+      return fetchFromFile();
+  }
+};
+
+export default fetchRooms;
